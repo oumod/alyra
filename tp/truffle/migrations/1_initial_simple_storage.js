37d4ea279c0da55d6c2ad552bf1126c9aa0c0fe5
Oudom Suos <oudom.suos@gmail.com>
2021-03-08 20:19:46 +0100

    Ajout du TP 'déployer sur le testnet avec Truffle'

---
+++ tp/truffle/migrations/1_initial_simple_storage.js
@@ -0,0 +1,5 @@
+const contract = artifacts.require("SimpleStorage");
+
+module.exports = function (deployer) {
+  deployer.deploy(contract);
+};
---
+++ tp/truffle/truffle-config.js
@@ -0,0 +1,56 @@
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * trufflesuite.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura accounts
+ * are available for free at: infura.io/register.
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+// Pour charger les informations sensibles depuis le fichier .env
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+
+  networks: {
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.ROPSTEN_URL),
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.RINKEBY_URL),
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 10000000000,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  compilers: {
+    solc: {
+      version: "0.6.11",    // version imposée par le contrat
+    }
+  }
+};
